efeb46a9a06d349c1fb1c41f0a4949f2ae000f85
Goran Bunic <goranb131@yahoo.com>
2026-09-14 12:47:55 +0200

    version bump (1.8.0)

--- src/prismafs.h
+++ src/prismafs.h
@@ -1,6 +1,6 @@
 /*
  * PrismaFS: A lightweight, layered filesystem.
- * Version: 1.7.1
+ * Version: 1.8.0
  * Copyright 2026 Goran Bunić
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
